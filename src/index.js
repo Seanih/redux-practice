f8152b2cda0c190ad9635318307b0b3316a6c820
Sean Anih <sean.anih@gmail.com>
2020-10-13 05:46:59 -0400

    successfully made store and dispatched an action

--- src/index.js
+++ src/index.js
@@ -3,6 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+
+
+
+
+//* Store = globallized state
+
+//? Action = describes what you want done (function that returns an object)
+
+const incrememnt = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrememnt = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+//* Reducer = checks Action and modifies Store accordingly
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+let store = createStore(counter)
+
+// display store in console
+store.subscribe(() => console.log(store.getState()))
+
+//! Dispatch = executes Action function(s)
+store.dispatch(incrememnt())
 
 ReactDOM.render(
   <React.StrictMode>
